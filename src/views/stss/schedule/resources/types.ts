f7c566283147346dffc1c081bfc6f190b82e3588
Eggnisi <3188064505@qq.com>
2026-06-21 16:02:16 +0800

    Revert "Feat/schedule"

--- src/views/stss/schedule/resources/types.ts
+++
@@ -1,8 +0,0 @@
-import type { Schedule } from "@/api/interface/schedule";
-
-export interface ResourceFilters {
-  category: Schedule.ResourceCategory;
-  department: string;
-  status: "" | Schedule.ResourceStatus;
-  keyword: string;
-}
